e235374ccdbe997cab16b771ca607f15d5c9132e
Noah Elias Kastner <elias@kastner-home.com>
2026-08-26 10:13:05 +0200

    finished python cheatsheet for unittest module

---
+++ python_unittest_module/src/main.py
@@ -0,0 +1,7 @@
+import calculator
+
+def main():
+    print(calculator.add(1, 2))
+
+if __name__ == "__main__":
+    main()
---
+++ python_unittest_module/test/test.py
@@ -0,0 +1,19 @@
+import unittest
+
+import sys
+sys.path.insert(0, "../src")
+from src import calculator
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(calculator.add(1, 2), 3)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(calculator.add(-1, -2), -3)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(calculator.add(1, -2), -1)
+        self.assertEqual(calculator.add(-1, 2), 1)
+
+if __name__ == '__main__':
+    unittest.main()
